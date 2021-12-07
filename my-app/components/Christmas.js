371070e0d1a386f2e3d6cbd687ae1f1858607e93
tanel.kossas <tanel.kossas@easypark.net>
2021-12-07 14:44:08 +0200

    Flash

--- my-app/components/Christmas.js
+++ my-app/components/Christmas.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Christmas = ({christmas}) => {
     return(
         <div className="container">
-            <h2>Is it T2 yet? {christmas.isItChristmasYet ? 'Yep' : 'Nope'}</h2>
+            <img src={'concise-6741-black.gif'}/>
         </div>
     )
 }
